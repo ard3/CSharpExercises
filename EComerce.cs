ea5407167ff2ddd98700e281f06548cc5558b271
ardit <arditllaha@gmail.com>
2020-11-28 23:15:01 +0100

    commit

--- EComerce.cs
+++ EComerce.cs
@@ -1,36 +1,58 @@
 namespace EComerce {
     class Costumer {
-        protected int id;
-        protected string name;
-        protected string surname;
-        protected string city;
-        protected int cap;
-        protected string email;
-        protected string password;
-            public void login()
-            {
-                Console.WriteLine("You are logged in ..");
-            }
-            public void acquista()
-            {
-                Console.WriteLine("Acquista");
-            }
-            public void iMieiOrdini()
-            {
-                Console.WriteLine("I miei ordini..");
-            }
-            public void WishList()
-            {
-                Console.WriteLine("wishList");
-            }
-            public void carrello()
-            {
-                Console.WriteLine("carrello");
-            }
-            public void registrazione()
-            {
-                Console.WriteLine("registrazione");
-            }
-    }
 
-}   
+             // Fields or Attributes
+        protected int Id;
+        protected string FirstName;
+        protected string LastName;
+        protected string Adress;
+        protected int PostalCode;
+        protected string Email;
+        protected string Password;
+
+        // Properties
+        // getter - setter
+
+        // Constructor
+        public Customer(string firstName, string lastName, string email)
+        {
+            // Called on instantiation
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+        }
+
+        // Methods
+        // visibilità - valore di ritorno - NomeMetodo()
+        public void Login()
+        {
+            Console.WriteLine("You are logged in ...");
+            Console.WriteLine($"Hi {this.FirstName} {this.LastName}, you are logged in.");
+        }
+        public void CheckOut()
+        {
+            Console.WriteLine("Product(s) purchased.");
+        }
+        public void MyOrders()
+        {
+            Console.WriteLine("These are your orders.");
+        }
+        public void WishList()
+        {
+            Console.WriteLine("This is your wishlist.");
+        }
+        public void AddToCart()
+        {
+            Console.WriteLine("Product added to cart!");
+        }
+        public void Signin()
+        {
+            Console.WriteLine("You are now signed in.");
+        }
+        public static void PrintSomething()
+        {
+            Console.WriteLine("Something");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -7,9 +7,30 @@ namespace EComerce
     {
         static void Main(string[] args)
         {
-            string myName = "ardit";
-            Console.WriteLine("Hello World!");
-            Console.WriteLine($"Il mio nome e {myName.ToUpper()}");
+            Console.WriteLine(args[0]);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i].ToUpper());
+            }
+
+            // var firstCustomer = new Customer();
+            Customer firstCustomer = new Customer("Francesco", "Bacci", "madcatxxx@gmail.com");
+            firstCustomer.Login();
+
+            Customer secondCustomer = new Customer("Mario", "Rossi", "m.rossi99@hotmail.com");
+            secondCustomer.Login();
+
+            Customer.PrintSomething();
+
+
+            /* Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+            //Console.WriteLine("Enter your name:");
+            //string tuoNome = Console.ReadLine();
+            //Console.WriteLine($"Il tuo nome è {tuoNome.ToUpper()}!");
+            Console.WriteLine("Enter your name:");
+            string tuoNome = Console.ReadLine();
+            Console.WriteLine($"Il tuo nome è {tuoNome.ToUpper()}!"); */
         }
     }
 }
